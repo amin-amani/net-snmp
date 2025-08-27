feeb260242224edd2df04579230eb70bf79d1549
amin-amani <a.amani@sinacomsys.com>
2025-08-27 16:51:05 +0330

    feat: wrte cmake for test

--- test/test_general.c
+++ test/test_general.c
@@ -4,14 +4,16 @@
 #include <stdio.h>
 #include <net-snmp/net-snmp-config.h>
 #include <net-snmp/net-snmp-includes.h>
-
-
+#include <net-snmp/agent/net-snmp-agent-includes.h>
+#include "agent/net-snmp-agent-includes.h"
 TEST_GROUP(GENERAL_GROUP);
 
 TEST_SETUP(GENERAL_GROUP)
 {
 //netsnmp_agent_session *asp;
 // netsnmp_add_queued(asp);
+//    netsnmp_agent_session *asp;
+//netsnmp_handle_request(asp,0);
 
 }
 
